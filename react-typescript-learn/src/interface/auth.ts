6bd870e77d48493d6361869a5737fecb92a856fe
quangle2003 <lequang20033@gmail.com>
2023-08-07 23:39:02 +0700

    signin,signup

---
+++ react-typescript-learn/src/interface/auth.ts
@@ -0,0 +1,6 @@
+export interface Auth {
+    name: string,
+    email: string,
+    password: number,
+    confirmPassword: number
+}
--- react-typescript-learn/src/store/index.ts
+++ react-typescript-learn/src/store/index.ts
@@ -1,13 +1,16 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import productApi, { productReducer } from "../api/product";
+import authApi from "../api/auth";
 
 export const store = configureStore({
     reducer: {
         [productApi.reducerPath]: productReducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
     middleware: getDefaultMiddleware => 
         getDefaultMiddleware()
             .concat(productApi.middleware)
+            .concat(authApi.middleware),
     
 })
 
